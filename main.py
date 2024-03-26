23462a274bfe204764daebaefd38596b7b28badc
Thomas Carson <thomas.carson2024@gmail.com>
2024-03-26 23:11:44 +0000

    began work on new movement system

--- main.py
+++ main.py
@@ -1,22 +1,153 @@
 import pygame as pg
-import sprites
 from sys import exit as sysexit
+from enum import Enum
 
 pg.init()
-screen = pg.display.set_mode((800, 800))
+WIDTH, HEIGHT = 442, 496
+screen = pg.display.set_mode((WIDTH, HEIGHT))
 pg.display.set_caption("Pac-Man")
 clock = pg.time.Clock()
+import sprites
 
+map_img: pg.Surface = pg.transform.scale2x(pg.image.load("sprites/map/map_empty.png").convert_alpha())
+map_rects: list[pg.Rect] = []
+
+class Direction(Enum):
+	UP = 0
+	DOWN = 1
+	LEFT = 2
+	RIGHT = 3
+
+	def flip(self): # I don't know if this works or not
+		if self.value == self.UP:
+			self.value = self.DOWN
+		elif self.value == self.DOWN:
+			self.value = self.UP
+		elif self.value == self.LEFT:
+			self.value = self.RIGHT
+		elif self.value == self.RIGHT:
+			self.value = self.LEFT
 
 class Pacman:
 	def __init__(self, pos: tuple[int, int]) -> None:
 		self.image: pg.Surface = sprites.Pacman.CIRCLE
-		self.rect = self.image.get_rect()
+		self.rect = self.image.get_rect(center = pos)
+		self.animation_type: sprites.PacmanAnimation = sprites.PacmanAnimation.RUN
+		self.direction: Direction = Direction.UP
+		self.animation_index = 0
+		self.queue: list[Direction] = []
+
+	def get_input(self) -> None:
+		keys = pg.key.get_pressed()
+		if keys[pg.K_UP] or keys[pg.K_w]:
+			if self.check_for_map_collisions(Direction.UP):
+				self.queue.append(Direction.UP)
+			else:
+				self.animation_type = sprites.PacmanAnimation.RUN
+				self.direction = Direction.UP
+				self.queue.clear()
+		elif keys[pg.K_DOWN] or keys[pg.K_s]:
+			if self.check_for_map_collisions(Direction.DOWN):
+				self.queue.append(Direction.DOWN)
+			else:
+				self.animation_type = sprites.PacmanAnimation.RUN
+				self.direction = Direction.DOWN
+				self.queue.clear()
+		elif keys[pg.K_LEFT] or keys[pg.K_a]:
+			if self.check_for_map_collisions(Direction.LEFT):
+				self.queue.append(Direction.LEFT)
+			else:
+				self.animation_type = sprites.PacmanAnimation.RUN
+				self.direction = Direction.LEFT
+				self.queue.clear()
+		elif keys[pg.K_RIGHT] or keys[pg.K_d]:
+			if self.check_for_map_collisions(Direction.RIGHT):
+				self.queue.append(Direction.RIGHT)
+			else:
+				self.animation_type = sprites.PacmanAnimation.RUN
+				self.direction = Direction.RIGHT
+				self.queue.clear()
+		else:
+			if len(self.queue) > 0 and self.check_for_map_collisions(self.queue[0]):
+				self.direction = self.queue.pop(0)
+
+	def animate(self) -> None:
+		if self.animation_type == sprites.PacmanAnimation.RUN:
+			self.animation_index += 1
+			if self.animation_index > 20:
+				self.animation_index = 0
+			match self.direction:
+				case Direction.UP:
+					self.image = sprites.Pacman.UP[int(self.animation_index >= 10)]
+				case Direction.DOWN:
+					self.image = sprites.Pacman.DOWN[int(self.animation_index >= 10)]
+				case Direction.LEFT:
+					self.image = sprites.Pacman.LEFT[int(self.animation_index >= 10)]
+				case Direction.RIGHT:
+					self.image = sprites.Pacman.RIGHT[int(self.animation_index >= 10)]
+
+	def move(self) -> None:
+		match self.direction:
+			case Direction.UP:
+				self.rect.y -= 2
+			case Direction.DOWN:
+				self.rect.y += 2
+			case Direction.LEFT:
+				self.rect.x -= 2
+			case Direction.RIGHT:
+				self.rect.x += 2
+		if self.rect.x < 0:
+			self.rect.x = WIDTH
+		if self.rect.x > WIDTH:
+			self.rect.x = 0
+		if self.rect.y < 0:
+			self.rect.y = HEIGHT
+		if self.rect.y > HEIGHT:
+			self.rect.y = 0
+
+	def  check_for_map_collisions(self, direction: Direction) -> bool:
+		map_colour = pg.Color(33, 33, 255)
+		match direction:
+			case Direction.UP:
+				axis = 1
+				offset = -1
+			case Direction.DOWN:
+				axis = 1
+				offset = 1
+			case Direction.LEFT:
+				axis = 0
+				offset = -1
+			case Direction.RIGHT:
+				axis = 0
+				offset = 1
+		buffer = 0
+		if axis == 0:
+			x = int(self.rect.centerx + (offset * (3+self.rect.width/2)))
+			for i in range(self.rect.top+buffer, self.rect.bottom-buffer):
+				try:
+					if screen.get_at([x, i]) == map_colour:
+						return True
+				except:
+					return False
+			return False
+		elif axis == 1:
+			y = int(self.rect.centery + (offset * (3+self.rect.height/2)))
+			for i in range(self.rect.left+buffer, self.rect.right-buffer):
+				try:
+					if screen.get_at([i, y]) == map_colour:
+						return True
+				except:
+					return False
+			return False
 
 	def update(self) -> None:
 		screen.blit(self.image, self.rect)
+		self.get_input()
+		self.animate()
+		if not self.check_for_map_collisions(self.direction):
+			self.move()
 
-pacman = Pacman()
+pacman = Pacman((WIDTH/2, HEIGHT/2))
 
 while True:
 	for event in pg.event.get():
@@ -24,7 +155,9 @@ while True:
 			pg.quit()
 			sysexit()
 
-	
+	screen.fill("black")
+	screen.blit(map_img, (0, 0))
+	pacman.update()
 
 	pg.display.update()
 	clock.tick(60)
--- sprites.py
+++ sprites.py
@@ -1,34 +1,35 @@
 import pygame as pg
 from enum import Enum
-
-class Pacman(Enum):
+class Pacman:
     UP: list[pg.Surface] = [
-        pg.image.load("sprites/pacman/up_0.png").convert_alpha(),
-        pg.image.load("sprites/pacman/up_1.png").convert_alpha()
+        pg.transform.scale2x(pg.image.load("sprites/pacman/run/up_0.png").convert_alpha()),
+        pg.transform.scale2x(pg.image.load("sprites/pacman/run/up_1.png").convert_alpha())
     ]
     DOWN: list[pg.Surface] = [
-        pg.image.load("sprites/pacman/down_0.png").convert_alpha(),
-        pg.image.load("sprites/pacman/down_1.png").convert_alpha()
+        pg.transform.scale2x(pg.image.load("sprites/pacman/run/down_0.png").convert_alpha()),
+        pg.transform.scale2x(pg.image.load("sprites/pacman/run/down_1.png").convert_alpha())
     ]
     LEFT: list[pg.Surface] = [
-        pg.image.load("sprites/pacman/left_0.png").convert_alpha(),
-        pg.image.load("sprites/pacman/left_1.png").convert_alpha()
+        pg.transform.scale2x(pg.image.load("sprites/pacman/run/left_0.png").convert_alpha()),
+        pg.transform.scale2x(pg.image.load("sprites/pacman/run/left_1.png").convert_alpha())
     ]
     RIGHT: list[pg.Surface] = [
-        pg.image.load("sprites/pacman/right_0.png").convert_alpha(),
-        pg.image.load("sprites/pacman/right_1.png").convert_alpha()
+        pg.transform.scale2x(pg.image.load("sprites/pacman/run/right_0.png").convert_alpha()),
+        pg.transform.scale2x(pg.image.load("sprites/pacman/run/right_1.png").convert_alpha())
     ]
-    CIRCLE: pg.Surface = pg.image.load("sprites/pacman/circle.png").convert_alpha()
-
-class PacmanDeath(Enum):
-    ONE: pg.Surface = pg.image.load("sprites/pacman/death/1.png").convert_alpha()
-    TWO: pg.Surface = pg.image.load("sprites/pacman/death/2.png").convert_alpha()
-    THREE: pg.Surface = pg.image.load("sprites/pacman/death/3.png").convert_alpha()
-    FOUR: pg.Surface = pg.image.load("sprites/pacman/death/4.png").convert_alpha()
-    FIVE: pg.Surface = pg.image.load("sprites/pacman/death/5.png").convert_alpha()
-    SIX: pg.Surface = pg.image.load("sprites/pacman/death/6.png").convert_alpha()
-    SEVEN: pg.Surface = pg.image.load("sprites/pacman/death/7.png").convert_alpha()
-    EIGHT: pg.Surface = pg.image.load("sprites/pacman/death/8.png").convert_alpha()
-    NINE: pg.Surface = pg.image.load("sprites/pacman/death/9.png").convert_alpha()
-    TEN: pg.Surface = pg.image.load("sprites/pacman/death/10.png").convert_alpha()
-    ELEVEN: pg.Surface = pg.image.load("sprites/pacman/death/11.png").convert_alpha()
+    CIRCLE: pg.Surface = pg.transform.scale2x(pg.image.load("sprites/pacman/circle.png").convert_alpha())
+class PacmanDeath:
+    ONE: pg.Surface = pg.transform.scale2x(pg.image.load("sprites/pacman/death/1.png").convert_alpha())
+    TWO: pg.Surface = pg.transform.scale2x(pg.image.load("sprites/pacman/death/2.png").convert_alpha())
+    THREE: pg.Surface = pg.transform.scale2x(pg.image.load("sprites/pacman/death/3.png").convert_alpha())
+    FOUR: pg.Surface = pg.transform.scale2x(pg.image.load("sprites/pacman/death/4.png").convert_alpha())
+    FIVE: pg.Surface = pg.transform.scale2x(pg.image.load("sprites/pacman/death/5.png").convert_alpha())
+    SIX: pg.Surface = pg.transform.scale2x(pg.image.load("sprites/pacman/death/6.png").convert_alpha())
+    SEVEN: pg.Surface = pg.transform.scale2x(pg.image.load("sprites/pacman/death/7.png").convert_alpha())
+    EIGHT: pg.Surface = pg.transform.scale2x(pg.image.load("sprites/pacman/death/8.png").convert_alpha())
+    NINE: pg.Surface = pg.transform.scale2x(pg.image.load("sprites/pacman/death/9.png").convert_alpha())
+    TEN: pg.Surface = pg.transform.scale2x(pg.image.load("sprites/pacman/death/10.png").convert_alpha())
+    ELEVEN: pg.Surface = pg.transform.scale2x(pg.image.load("sprites/pacman/death/11.png").convert_alpha())
+class PacmanAnimation(Enum):
+    RUN = 0
+    DEATH = 1
