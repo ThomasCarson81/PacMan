593f8b4178d927742c3758b6f75f058b7f44080c
Thomas Carson <thomas.carson2024@gmail.com>
2024-03-26 12:49:30 +0000

    type annotated all surfaces

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame as pg
+import sprites
 from sys import exit as sysexit
 
 pg.init()
@@ -8,18 +9,22 @@ clock = pg.time.Clock()
 
 
 class Pacman:
-	def __init__(self) -> None:
-		self.image = pg.Surface((50,50))
+	def __init__(self, pos: tuple[int, int]) -> None:
+		self.image: pg.Surface = sprites.Pacman.CIRCLE
 		self.rect = self.image.get_rect()
 
 	def update(self) -> None:
 		screen.blit(self.image, self.rect)
+
+pacman = Pacman()
 
 while True:
 	for event in pg.event.get():
 		if event.type == pg.QUIT:
 			pg.quit()
 			sysexit()
+
+	
 
 	pg.display.update()
 	clock.tick(60)
--- sprites.py
+++ sprites.py
@@ -2,33 +2,33 @@ import pygame as pg
 from enum import Enum
 
 class Pacman(Enum):
-    UP = [
+    UP: list[pg.Surface] = [
         pg.image.load("sprites/pacman/up_0.png").convert_alpha(),
         pg.image.load("sprites/pacman/up_1.png").convert_alpha()
     ]
-    DOWN = [
+    DOWN: list[pg.Surface] = [
         pg.image.load("sprites/pacman/down_0.png").convert_alpha(),
         pg.image.load("sprites/pacman/down_1.png").convert_alpha()
     ]
-    LEFT = [
+    LEFT: list[pg.Surface] = [
         pg.image.load("sprites/pacman/left_0.png").convert_alpha(),
         pg.image.load("sprites/pacman/left_1.png").convert_alpha()
     ]
-    RIGHT = [
+    RIGHT: list[pg.Surface] = [
         pg.image.load("sprites/pacman/right_0.png").convert_alpha(),
         pg.image.load("sprites/pacman/right_1.png").convert_alpha()
     ]
-    CIRCLE = pg.image.load("sprites/pacman/circle.png").convert_alpha()
+    CIRCLE: pg.Surface = pg.image.load("sprites/pacman/circle.png").convert_alpha()
 
 class PacmanDeath(Enum):
-    ONE = pg.image.load("sprites/pacman/death/1.png").convert_alpha()
-    TWO = pg.image.load("sprites/pacman/death/2.png").convert_alpha()
-    THREE = pg.image.load("sprites/pacman/death/3.png").convert_alpha()
-    FOUR = pg.image.load("sprites/pacman/death/4.png").convert_alpha()
-    FIVE = pg.image.load("sprites/pacman/death/5.png").convert_alpha()
-    SIX = pg.image.load("sprites/pacman/death/6.png").convert_alpha()
-    SEVEN = pg.image.load("sprites/pacman/death/7.png").convert_alpha()
-    EIGHT = pg.image.load("sprites/pacman/death/8.png").convert_alpha()
-    NINE = pg.image.load("sprites/pacman/death/9.png").convert_alpha()
-    TEN = pg.image.load("sprites/pacman/death/10.png").convert_alpha()
-    ELEVEN = pg.image.load("sprites/pacman/death/11.png").convert_alpha()
+    ONE: pg.Surface = pg.image.load("sprites/pacman/death/1.png").convert_alpha()
+    TWO: pg.Surface = pg.image.load("sprites/pacman/death/2.png").convert_alpha()
+    THREE: pg.Surface = pg.image.load("sprites/pacman/death/3.png").convert_alpha()
+    FOUR: pg.Surface = pg.image.load("sprites/pacman/death/4.png").convert_alpha()
+    FIVE: pg.Surface = pg.image.load("sprites/pacman/death/5.png").convert_alpha()
+    SIX: pg.Surface = pg.image.load("sprites/pacman/death/6.png").convert_alpha()
+    SEVEN: pg.Surface = pg.image.load("sprites/pacman/death/7.png").convert_alpha()
+    EIGHT: pg.Surface = pg.image.load("sprites/pacman/death/8.png").convert_alpha()
+    NINE: pg.Surface = pg.image.load("sprites/pacman/death/9.png").convert_alpha()
+    TEN: pg.Surface = pg.image.load("sprites/pacman/death/10.png").convert_alpha()
+    ELEVEN: pg.Surface = pg.image.load("sprites/pacman/death/11.png").convert_alpha()
